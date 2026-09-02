4fb5a41f5ec4e74182e27d918fbc05023c90cdd0
Anthony Oparaugo <106179293+PEP-Tony@users.noreply.github.com>
2026-09-02 09:49:31 +0100

    Create get_single.js

---
+++ routes/get_single.js
@@ -0,0 +1,12 @@
+const express = require ("express");
+const router = express.Router();
+const inventory = require("./data/inventory");
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    const product = inventory.find((t) => t.id === parseInt(id));
+    if(!product) return res.status(404).json({error: "Not found"});
+    res.status(200).json(product);
+});
+
+module.exports = router;

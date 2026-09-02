5c651351068b8514b549e9b95ba2426580d06ac3
Eric-zard <wanderaerickson01@gmail.com>
2026-09-02 11:05:14 +0300

    added patch route

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const patchProduct = require("./routes/patch");
 const errorHandler = require("./middleware/error_handler");
 const inventory = require("./routes/data/inventory");
 
@@ -7,6 +8,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.use('/', patchProduct);
 
 // Error handler must be last
 app.use(errorHandler);
